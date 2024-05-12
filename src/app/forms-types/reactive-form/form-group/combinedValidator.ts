350835b1be42cc4f896c2b62c5763b0cac194732
Msuf123 <akshatmalik18t@gmail.com>
2024-05-12 15:16:04 +0530

    Started learning cross feild validation

--- src/app/forms-types/reactive-form/form-group/combinedValidator.ts
+++ src/app/forms-types/reactive-form/form-group/combinedValidator.ts
@@ -1,11 +1,11 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export const identityRevealedValidator: ValidatorFn = (
     control: AbstractControl,
   ): ValidationErrors | null => {
     const name = control.get('name');
     
-   console.log('me')
+   console.log(name?.value)
     return name  && name.value === 'AkshatMalik'
       ? { identityRevealed: true }
       : null;
--- src/app/forms-types/reactive-form/form-group/form-group.component.ts
+++ src/app/forms-types/reactive-form/form-group/form-group.component.ts
@@ -15,57 +15,50 @@ export class FormGroupComponent {
   showTheDomain=false
   showPassError=false
   error=''
-  formGroup=this.formBuilder.group({
-    name:['',[Validators.required]],
-    password:['',[Validators.minLength(6)]],
-    hobbies:this.formBuilder.array([]),
-    address:this.formBuilder.group({
-      address:['',[Validators.required]],
-      state:['',[Validators.required]],
-      country:['',[Validators.required]],
-    }),
-    coder:['no'],
-    domain:['']
-  },{Validators:identityRevealedValidator})
+  formGroup=new FormGroup({
+    name:new FormControl('',{validators:[Validators.required]}),
+    password:new FormControl('',{validators:[Validators.minLength(6)]}),
+   
+  },{validators:[identityRevealedValidator]})
   // jack=new FormGroup({name:new FormControl('')},{validators:})
  constructor(private formBuilder:FormBuilder) {
   this.formGroup.statusChanges.subscribe((a)=>{
-    
+    console.log(this.formGroup.errors)
     let errors=this.formGroup.get('password')?.errors
     console.log(errors)
     if(errors){
-      console.log('Status vhanged')
+     
      this.showPassError=true
       for(let a in errors){
       this.error=errors[a]['requiredLength']
     }
   }
   })
-  this.formGroup.valueChanges.subscribe((a)=>{
-    if(a.coder==='yes'){
+//   this.formGroup.valueChanges.subscribe((a)=>{
+//     if(a.coder==='yes'){
       
-      this.showTheDomain=true
-    }
-    else{
-      this.showTheDomain=false
-    }
-  })
- }
- adHobbies(){
+//       this.showTheDomain=true
+//     }
+//     else{
+//       this.showTheDomain=false
+//     }
+//   })
+  }
+//  adHobbies(){
   
-  this.arrayOfForm.push(new FormControl(''))
- }
- get arrayOfForm(){
-    return  this.formGroup.get('hobbies') as FormArray
- }
+//   this.arrayOfForm.push(new FormControl(''))
+//  }
+//  get arrayOfForm(){
+//     return  this.formGroup.get('hobbies') as FormArray
+//  }
  formSubmit(){
   console.log(this.formGroup.value)
  }
- ngDoCheck(){
-  }
-  heIsaCoder(){
-    if(this.formGroup.value.coder==='yes'){
-      this.formGroup.patchValue({domain:''})
-    }
-  }
+//  ngDoCheck(){
+//   }
+//   heIsaCoder(){
+//     if(this.formGroup.value.coder==='yes'){
+//       this.formGroup.patchValue({domain:''})
+//     }
+ // }
 }
