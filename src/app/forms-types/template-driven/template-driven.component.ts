2f2add4de0caa10ddabc5bd6321e8cb17c3e5304
Msuf123 <akshatmalik18t@gmail.com>
2024-05-13 18:17:08 +0530

    Understood template driven form but the validation is left

--- src/app/forms-types/template-driven/template-driven.component.ts
+++ src/app/forms-types/template-driven/template-driven.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './template-driven.component.html',
   styleUrl: './template-driven.component.css'
 })
 export class TemplateDrivenComponent {
-
+ value=''
+ models=null
+ 
+ changes(){
+  console.log(this.value)
+ }
 }
