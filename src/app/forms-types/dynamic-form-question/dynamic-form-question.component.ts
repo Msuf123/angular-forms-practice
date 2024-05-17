2a57edc32fc6b2afcbb190c27024828bfd905030
Msuf123 <akshatmalik18t@gmail.com>
2024-05-17 16:03:07 +0530

    so first we created a service which will make the formGroup then we made a componet to show that goup

--- src/app/forms-types/dynamic-form-question/dynamic-form-question.component.ts
+++ src/app/forms-types/dynamic-form-question/dynamic-form-question.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { QuestionBase } from '../dynamic-form/question-base';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dynamic-form-question',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './dynamic-form-question.component.html',
   styleUrl: './dynamic-form-question.component.css'
 })
 export class DynamicFormQuestionComponent {
-
+  @Input() question!:QuestionBase<string>
+  @Input() form!:FormGroup
+  get isValid(){
+     return this.form.controls[this.question.key].valid;
+  }
 }
