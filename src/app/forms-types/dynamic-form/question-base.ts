d3c9643a3de627ecb59e7c741207b598fb54e7ee
Msuf123 <akshatmalik18t@gmail.com>
2024-05-17 13:35:00 +0530

    In our questionBase we had different properties now we create two classes that extend the question base and overide the controlTypr property

--- src/app/forms-types/dynamic-form/question-base.ts
+++ src/app/forms-types/dynamic-form/question-base.ts
@@ -1,3 +1,32 @@
 export class QuestionBase<T>{
-    
+    value:T|undefined
+    key:string
+    label:string
+    required:boolean
+    order:number
+    controlType:string
+    type:string
+    options:{key:string,value:string}[]
+    constructor(options:{
+        value?:T;
+        key?:string;
+        lable?:string;
+        required?:boolean;
+        order?:number;
+        controlType?:string;
+        type?:string;
+        options?:{key:string,value:string}[];
+    }={}){
+        this.value=options.value
+        this.key=options.key||''
+        this.label=options.lable||''
+        this.required=!!options.required
+        this.order=options.order===undefined?1:options.order
+        this.controlType=options.controlType||''
+        this.type=options.type||''
+        this.options=options.options||[]
+
+
+
+    }
 }
