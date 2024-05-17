7bc24c8a17ce720edc18b07de5b8c8c508d3161e
Msuf123 <akshatmalik18t@gmail.com>
2024-05-17 14:56:38 +0530

    Created a service that loops through array of question class and then appnds them in the obj of type any and we are inserting key as the key of each formControl in the formGroup obj then at end we are appending the obj in the formGroup and returning it

--- src/app/service/forms-service/question-control.service.ts
+++ src/app/service/forms-service/question-control.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { QuestionBase } from '../../forms-types/dynamic-form/question-base';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,11 @@ import { Injectable } from '@angular/core';
 export class QuestionControlService {
 
   constructor() { }
+  toFormGroup(questions:QuestionBase<string>[]){
+    const group:any={}
+    questions.forEach((question)=>{
+      group[question.key]=question.required?new FormControl(question.value||'',Validators.required):new FormControl(question.value||'')
+    })
+    return new FormGroup(group)
+  }
 }
